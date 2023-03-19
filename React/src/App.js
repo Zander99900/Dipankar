f8e148af8c58bc83a704b0add6f6dd78bcfc16d9
Zander99900 <dipankarkarmakardk007@gmail.com>
2023-03-19 20:34:16 +0530

    Learned States

--- React/src/App.js
+++ React/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Navbar from './Navbar';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
     //This will help organise codes in a clean way
     <>
       <Navbar Title = 'TextUtils' aboutText = 'About us'/>
+      <div className="container"><TextForm heading = "Text Conversion Website"/></div>
     </>
   );
 }
