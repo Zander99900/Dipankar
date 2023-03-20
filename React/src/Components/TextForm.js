d66cdb4974168b66b17e6af2483caa8b31effcc7
Zander99900 <dipankarkarmakardk007@gmail.com>
2023-03-20 19:38:25 +0530

    Navbar dark Mode

--- React/src/Components/TextForm.js
+++ React/src/Components/TextForm.js
@@ -6,28 +6,45 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     };
+    const handleLowClick = () => {
+        console.log("UpperCase Was Clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    };
 
     const handleOnClick = (Event) => {
         console.log("On Change");
         setText(Event.target.value);
     };
-    const [text, setText] = useState("Enter Text Here");
+    const [text, setText] = useState("");
 
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea
-                    className="form-control"
-                    value={text}
-                    onChange={handleOnClick}
-                    id="myBox"
-                    rows="8"
-                ></textarea>
-            </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>
-                Convert To Uppercase
-            </button>
+        <>
+        <div className="container">
+          <h1>{props.heading}</h1>
+          <div className="mb-3">
+            <textarea
+              className="form-control"
+              value={text}
+              onChange={handleOnClick}
+              id="myBox"
+              rows="8"
+            ></textarea>
+          </div>
+          <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+            Convert To Uppercase
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+            Convert To LowerCase
+          </button>
         </div>
+        <div className="container my-3">
+          <h2>Your text summary</h2>
+          <p>{text.split(" ").length} words and {text.length} characters including Spaces</p>
+          <h2>Preview</h2>
+          <p>{text}</p>
+        </div>
+      </>
     );
-}
+    //  primary for blue, success for green & danger for red - bootstrap keyword
+  }
