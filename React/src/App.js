f81213a6dbb3c960620d4bd974623f51d69cccaf
Zander99900 <dipankarkarmakardk007@gmail.com>
2023-03-20 20:14:06 +0530

    Added About page with dark mode

--- React/src/App.js
+++ React/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
+// import TextForm from './Components/TextForm';
+import About from './Components/About';
 
 function App() {
   return (
     //This will help organise codes in a clean way
     <>
       <Navbar Title = 'TextUtils' aboutText = 'About us'/>
-      <div className="container"><TextForm heading = "Text Conversion Website"/></div>
+      {/* { <div className="container"><TextForm heading = "Text Conversion Website"/></div>} */}
+      { <div className="container"> <About/></div>}
+
     </>
   );
 }
