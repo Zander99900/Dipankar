55b68ab90dd766814fe7036598c3894542f99c42
Zander99900 <dipankarkarmakardk007@gmail.com>
2023-04-01 20:44:13 +0530

    Added Links to NewsItem Cards

--- React/NewsApp/src/components/NewsItem.js
+++ React/NewsApp/src/components/NewsItem.js
@@ -2,20 +2,20 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imgUrl, newsUrl } = this.props;
     return (
       <div className="card" style={{ width: "18rem" }}>
         <img
           className="card-img-top"
-          src="https://images.axios.com/vsDiOj0mAxxNK5WxJ4kSc7edy7E=/0x0:1920x1080/1366x768/2023/03/27/1679925999384.jpg"
+          src={imgUrl}
           alt="Card cap"
         />
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-          <a href="/newsdetails" className="btn btn-primary">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <a href={newsUrl} target = '_blank' rel="noreferrer" className="btn btn-primary btn-sm ">
             {/* /newsdetails is just a temporary link which doesnt exist and added only to remove empty link error in console */}
-            Go somewhere
+            Read more
           </a>
         </div>
       </div>
